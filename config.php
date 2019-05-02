7965cb398c38cda17970618ec0b66422084b39a4
sidglas <sidglas@hotmail.com>
2019-05-01 22:23:09 -0300

    Iniciação do Projeto fase 2

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class_name){
 
-
+	//ajuste para procurar em /class
 	$fileName = "class". DIRECTORY_SEPARATOR .$class_name . ".php";
 
 	if (file_exists($fileName)) {
